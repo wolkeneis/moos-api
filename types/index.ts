b689c2b259a5b3a9a56f101b47c91c24382509b0
Simon <simon@benezan.de>
2022-05-15 08:30:03 +0200

    test enum in api definition

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,13 @@
-import * as v1 from "../reference/moos-api-v1";
+import * as generatedV1 from "../reference/moos-api-v1";
+
+enum AuthScope {
+  identify = "identify",
+  all = "*"
+}
+
+const v1 = {
+  ...generatedV1,
+  AuthScope: AuthScope
+};
 
 export { v1 };
