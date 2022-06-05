5db2f4e8b48a315ee2db8cd94cc5e9f0fcdb7928
Simon <simon@benezan.de>
2022-06-05 07:18:24 +0200

    file component export

--- types/index.ts
+++ types/index.ts
@@ -7,12 +7,14 @@ declare namespace v1 {
 
   export enum AuthScope {
     identify = "identify",
+    files = "*",
     all = "*"
   }
 
   export type UserProfile = _components["schemas"]["UserProfile"];
   export type ProviderProfile = _components["schemas"]["ProviderProfile"];
   export type Application = _components["schemas"]["Application"];
+  export type File = _components["schemas"]["File"];
 }
 
 export { v1 };
